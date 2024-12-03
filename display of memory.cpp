226d71a38e5c66368e6151d4353217648350ddbc
S Rain <10245102440@stu.ecnu.edu.cn>
2024-12-03 11:12:01 +0800

    Add files via upload

--- display of memory.cpp
+++ display of memory.cpp
@@ -1,8 +1,9 @@
-//
+/*一个 int 类型变量或 double 类型变量在连续几个字节的内存中存放。读取数值时，当数值中包含小数点时类型为 double，否则类型为 int。
+将读入的数值存放在 int 类型变量或 double 类型变量中。以十六进制格式（a-f 字母采用小写形式）输出相应变量的连续的每个字节的值。*/
 #include <iostream>
 using namespace std;
 int main()
 {
-    
+
     return 0;
 }
